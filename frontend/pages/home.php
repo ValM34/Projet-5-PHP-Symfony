c261d18ba9e70fcbcafc8dcd1804f4ae1bcfe9af
Valentin MOREAU <vmoreau@datasolution.fr>
2022-06-17 14:53:46 +0200

    test commit gitignore

---
+++ frontend/pages/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    <title>blog</title>
+</head>
+
+<body>
+    <header>
+        <a href="#" class="h-logo">Logo</a>
+        <div class="h-navlinks">
+            <div class="h-navlinks-left">
+                <a href="#" class="h-navlink">Accueil</a>
+                <a href="#" class="h-navlink">Articles</a>
+            </div>
+            <div class="h-navlinks-right">
+                <a href="#" class="h-navlink">Profil</a>
+            </div>
+        </div>
+    </header>
+    <section>
+
+    </section>
+
+
+    <footer>
+
+    </footer>
+
+
+
+
+</body>
+
+</html>
